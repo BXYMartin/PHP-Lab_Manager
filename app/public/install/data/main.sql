92e50382bf8898d414d2a0a1f9c22261f486346d
BXYMartin <BXYMartin@hotmail.com>
2019-12-14 17:44:43 +0800

    quick fix

--- app/public/install/data/main.sql
+++ app/public/install/data/main.sql
@@ -700,7 +700,7 @@ CREATE TABLE `issue_type` (
   `font_awesome` varchar(20) DEFAULT NULL,
   `custom_icon_url` varchar(128) DEFAULT NULL,
   `is_system` tinyint(1) UNSIGNED DEFAULT '0',
-  `form_desc_tpl_id` int(11) UNSIGNED DEFAULT '0' COMMENT '创建事项时,描述字段对应的模板id'
+  `form_desc_tpl_id` int(11) UNSIGNED DEFAULT '0' COMMENT '创建事项时,描述字段对应的模板id',
   `is_collect_issue` tinyint(1) unsigned DEFAULT '1',
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
